c0de2e91cfa9822ac7b82d94d6fa5590c0e0b42a
DiasKzStrong <dias.ziyada12345@gmail.com>
2023-03-20 17:49:34 +0600

    adding comment to urls

--- Test/testing/urls.py
+++ Test/testing/urls.py
@@ -11,23 +11,23 @@ router.register(r'comments',CommentsView,basename='comment')
 app_name = 'api'
 urlpatterns = [
     
-    path('tests',TestView.as_view(),name='tests'),
-    path('category',CategoryView.as_view(),name='category'),
-    path('category/<str:name>',CategorySingleView.as_view(),name='categorysingle'),
-    path('questions',QuestionsView.as_view(),name='questions'),
-    path('users',UserView.as_view(),name='users'),
-    path('users/me',UserMeView.as_view(),name='me'),
-    path('users/<str:username>',UserSingleView.as_view(),name='usersingle'),
-    path('userstats',UserStatisticsView.as_view(),name='userstats'),
-    path('userstats/<str:user__username>',UserStatisticsSingleView.as_view(),name='userstatssingle'),
-    path('user/stats/me',UserStatisticsMeView.as_view(),name='userstatsme'),
-    path('userprofile/<str:user_username>',UserProfileSingleView.as_view(),name='user-profile'),
-    path('user/profile/me',UserProfileMeView.as_view(),name='userprofileme'),
-    path('login',user_login,name='login'),
-    path('logout',user_logout,name='logout'),
-    path('register',UserRegistrationView.as_view(),name='register'),
+    path('tests',TestView.as_view(),name='tests'), #Тесттар 
+    path('category',CategoryView.as_view(),name='category'),# Категориялар
+    path('category/<str:name>',CategorySingleView.as_view(),name='categorysingle'), # Категорияларга ид аркылы киресин
+    path('questions',QuestionsView.as_view(),name='questions'),# каждый сурак
+    path('users',UserView.as_view(),name='users'),# Букил юзерлар
+    path('users/me',UserMeView.as_view(),name='me'),# Аккаунтка кирген адамның информациясы
+    path('users/<str:username>',UserSingleView.as_view(),name='usersingle'),# Каждый юзерге аты бойынша киресин
+    path('userstats',UserStatisticsView.as_view(),name='userstats'),# Юзерлардың статистикасы
+    path('userstats/<str:user__username>',UserStatisticsSingleView.as_view(),name='userstatssingle'),# юзер статистакасына аты бойынша киресин
+    path('user/stats/me',UserStatisticsMeView.as_view(),name='userstatsme'),# нау кирген юзердың статистикасы
+    path('userprofile/<str:user_username>',UserProfileSingleView.as_view(),name='user-profile'),# нау адамның типо профильы
+    path('user/profile/me',UserProfileMeView.as_view(),name='userprofileme'),# кирген адамның профильы
+    path('login',user_login,name='login'),# логин 
+    path('logout',user_logout,name='logout'),# шыгу 
+    path('register',UserRegistrationView.as_view(),name='register'),# регистрациясы
     
     #viewsets 
-    path('',include(router.urls)),
+    path('',include(router.urls)),# на жерде тесттар и лайктар и комментар
     
 ]
